c3c8f9cbc22e6b72d93386fd42860db491fa7e3a
jayakrishnapalla05 <jayakrishnapalla05@gmail.com>
2023-08-27 13:33:04 +0530

    submitting code-Sun Aug 27 2023 13:33:03 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,34 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+const [tab,setTab]=useState("Tab 1");
+const [tab2,setTab2]=useState("Tab A");
+
+function handleClick(){
+  setTab(e.target.innerText);
+}
+function handleClick2(){
+  setTab2(e.target.innerText);
+}
+
+
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <ul>
+          <li onChange={handleClick}>Tab 1</li>
+           <li onChange={handleClick}>Tab 2</li>
+            <li onChange={handleClick}>Tab 3</li>
+
+        </ul>
+        <p>Content for {tab}</p>
+        <ul>
+          <li onChange={handleClick2}>Tab A</li>
+          <li onChange={handleClick2}>Tab B</li>
+          <li onChange={handleClick2}>Tab C</li>
+        </ul>
+        <p>Content for {tab2}</p>
     </div>
   )
 }
